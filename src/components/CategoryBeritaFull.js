cb538d4729813d5ffe37c3949727e5e51d7922ef
busetdah <apriantowpj@gmail.com>
2025-01-21 23:05:06 +0700

    CategoryBeritaFull: make results show on list berita

--- src/components/CategoryBeritaFull.js
+++ src/components/CategoryBeritaFull.js
@@ -76,111 +76,92 @@ function CategoryBeritaFull() {
     return <div>Berita Tidak Ditemukan !</div>;
   }
 
+  const displayedBerita = searchQuery.trim() !== "" ? searchResults : listBerita;
+
   return (
-      <div className="grid grid-cols-12 px-4 p-12 mt-20">
-        <div className="col-start-2 col-span-2 text-lg mb-4">
-          <Link to="/berita">Berita</Link> {" > "}
-          <span className="blue">{category.name}</span>
+    <div className="grid grid-cols-12 px-4 p-12 mt-20">
+      <div className="col-start-2 col-span-2 text-lg mb-4">
+        <Link to="/berita">Berita</Link> {" > "}
+        <span className="blue">{category.name}</span>
+      </div>
+      <div className="col-start-3 col-span-2">
+        <div className="relative mt-10">
+          <div className="absolute hr-orange h-7 w-14 top-2 -left-4 -z-10"></div>
+          <p className="text-2xl font-bold">
+            Menu {" "}
+            <span className="blue">Berita ZI-RB</span>
+          </p>
+          <div className="border-2 border-black mt-2 w-full"></div>
         </div>
-        <div className="col-start-3 col-span-2">
-          <div className="relative mt-10">
-            <div className="absolute hr-orange h-7 w-14 top-2 -left-4 -z-10"></div>
-            <p className="text-2xl font-bold">
-              Menu {" "}
-              <span className="blue">Berita ZI-RB</span>
-            </p>
-            <div className="border-2 border-black mt-2 w-full"></div>
-          </div>
-          <div className="mt-4 space-y-4">
-            {categories.map((item) =>
-              parseInt(id) === item.id ? (
-                <div key={item.id}>
-                  <span className="block font-semibold text-lg blue">
-                    {item.name}
-                  </span>
-                  <div className="ml-4 mt-2 space-y-2">
-                    {item.subcategories.map((subcategory) => (
-                      <span
-                        key={subcategory.id}
-                        className="block text-gray-700"
-                      >
-                        {subcategory.name}
-                      </span>
-                    ))}
-                  </div>
-                </div>
-              ) : (
-                <Link
-                  key={item.id}
-                  to={`/menu-berita/${item.id}`}
-                  className="block font-semibold text-lg underline hover:text-blue-800"
-                >
+        <div className="mt-4 space-y-4">
+          {categories.map((item) =>
+            parseInt(id) === item.id ? (
+              <div key={item.id}>
+                <span className="block font-semibold text-lg blue">
                   {item.name}
-                </Link>
-              )
-            )}
-          </div>
-        </div>
-        <div className="col-start-9 col-span-3 mt-4 row-start-1">
-          <div className="relative w-full">
-            <i className="fa fa-search absolute left-3 top-2.5 text-gray-500"></i>
-            <input
-              type="text"
-              className="border-2 border-black rounded-full px-8 py-1 w-full"
-              placeholder="Cari judul berita disini"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-          </div>
-          {searchResults.length > 0 ? (
-            <div className="mt-4">
-              <p className="font-semibold">Hasil Pencarian:</p>
-              <ul className="list-disc list-inside">
-                {searchResults.map((result) => (
-                  <li key={result.id} className="text-gray-700">
-                    <Link to={result.link} className="underline hover:text-blue-800">
-                      {result.judul}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ) : (
-            searchQuery && (
-              <p className="mt-4 text-gray-500">Tidak ada hasil yang cocok.</p>
+                </span>
+                <div className="ml-4 mt-2 space-y-2">
+                  {item.subcategories.map((subcategory) => (
+                    <span
+                      key={subcategory.id}
+                      className="block text-gray-700"
+                    >
+                      {subcategory.name}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <Link
+                key={item.id}
+                to={`/menu-berita/${item.id}`}
+                className="block font-semibold text-lg underline hover:text-blue-800"
+              >
+                {item.name}
+              </Link>
             )
           )}
         </div>
-        <div className="col-start-6 col-span-6 row-start-2">
-        {listBerita.map((item) => (
-        <div className="mt-8">
-        <a
-          href={item.link}
-          className="flex items-center"
-        >
-           <div className="flex p-4rounded-lg space-x-4 max-w-4xl">
-      <div className="w-32 h-32 flex-shrink-0">
-        <img
-          src={item.image}
-          alt="Thumbnail Berita"
-          className="w-full h-full object-cover"
-        />
       </div>
-      <div className="flex flex-col">
-        <h2 className="text-xl font-bold mb-2">{item.judul}</h2>
-        <p className="text-black font-normal mb-4 leading-tight">
-        {item.desc}
-        </p>
-        <p className="text-right mr-12 hover:underline">
-          Lihat Selengkapnya ➔
-        </p>
+      <div className="col-start-9 col-span-3 mt-4 row-start-1">
+        <div className="relative w-full">
+          <i className="fa fa-search absolute left-3 top-2.5 text-gray-500"></i>
+          <input
+            type="text"
+            className="border-2 border-black rounded-full px-8 py-1 w-full"
+            placeholder="Cari judul berita disini"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="col-start-6 col-span-6 row-start-2">
+        {displayedBerita.map((item) => (
+          <div key={item.id} className="mt-8">
+            <a href={item.link} className="flex items-center">
+              <div className="flex p-4 rounded-lg space-x-4 max-w-4xl">
+                <div className="w-32 h-32 flex-shrink-0">
+                  <img
+                    src={item.image}
+                    alt="Thumbnail Berita"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <h2 className="text-xl font-bold mb-2">{item.judul}</h2>
+                  <p className="text-black font-normal mb-4 leading-tight">
+                    {item.desc}
+                  </p>
+                  <p className="text-right mr-12 hover:underline">
+                    Lihat Selengkapnya ➔
+                  </p>
+                </div>
+              </div>
+            </a>
+          </div>
+        ))}
       </div>
     </div>
-    </a>
-        </div>
-        ))}
-        </div>
-      </div>
   );
 }
 
