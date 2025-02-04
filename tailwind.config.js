f96021e7bc007c69dc3dc368e6562f14c8b83450
busetdah <apriantowpj@gmail.com>
2025-02-04 20:45:38 +0700

    adapt custom \color configuration on tailwind with old website

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pdarkblue: "#0B588F",
+        pblue: "#26AAE1",
+        porange: "#EB891B",
+        pgreen: "#68B92E",
+        base: "#EEF0F2",
+        ternaryBlue: "#0A3357",
+        primaryBlue: "#228be6",
+        secondaryBlue: "#D7EAFB",
+        primaryOrange: "#e67e22",
+        grayCustom: "#eef0f2",
+      },
+    },
   },
   plugins: [],
 };
